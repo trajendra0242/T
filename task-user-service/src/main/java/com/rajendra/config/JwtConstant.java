c9a63d52d56ab3ce61065d98c0d469359629ac68
Rajendra <trajendra0242@gmail.com>
2024-12-06 18:03:13 +0530

    Security part done in taskuser project

---
+++ task-user-service/src/main/java/com/rajendra/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.rajendra.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY="jfsljflksjflksjflksjflkjflkdsjflfjkljfalsjf";
+    public static final String JWT_HEADE="Authorization";
+}
